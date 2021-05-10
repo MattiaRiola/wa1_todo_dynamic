638d526f0d10f5bcbaede6d2c10d36fdb4bad09a
Bacarus <code.bacarus@gmail.com>
2021-05-10 15:09:45 +0200

    feat: add basics api all tasks query

---
+++ server/dao.js
@@ -0,0 +1,24 @@
+'use strict';
+/* Data Access Object (DAO) module for accessing courses and exams */
+
+const sqlite = require('sqlite3');
+
+// open the database
+const db = new sqlite.Database('tasks.db', (err) => {
+  if(err) throw err;
+});
+
+// get all tasks
+exports.listTasks = () => {
+    return new Promise((resolve, reject) => {
+      const sql = 'SELECT * FROM tasks';
+      db.all(sql, [], (err, rows) => {
+        if (err) {
+          reject(err);
+          return;
+        }
+        const tasks = rows.map((t) => ({ id: t.id, description: t.description, important: t.important, private: t.private, deadline: t.deadline, completed: t.completed, user: t.user }));
+        resolve(tasks);
+      });
+    });
+};
--- server/server.js
+++ server/server.js
@@ -1,7 +1,27 @@
-const express = require('express');
+'use strict';
+
+const express = require('express') ;
+const morgan = require('morgan') ;
 
 const PORT = 3001;
 
-app = new express();
+const dao = require('./dao');
+
+const app = express();
+
+app.use(morgan('dev')) ;
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World, from you server');
+}) ;
+
+app.get('/api/alltasks', (req, res) => {
+    dao.listTasks()
+        .then((tasks) => { res.json(tasks); })
+        .catch((error) => {res.status(500).json(error); });
+}) ;
+
 
 app.listen(PORT, ()=>console.log(`Server running on http://localhost:${PORT}/`));
